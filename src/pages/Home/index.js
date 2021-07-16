a289e37ffce4297f68e58129617c30fa4a4627fe
ilaylow <chuenleylow@gmail.com>
2021-07-17 04:33:07 +1000

    added in fade in

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,18 @@
 import styles from "./Home.module.scss";
 import hackmelb_logo from "../../images/hm_dark_logo 2.svg";
 import arrow_logo from "../../images/light-arrow.svg";
+import FadeIn from 'react-fade-in';
 
 const Home = () => {
   return (
   <div className={styles.container}>
     <img className = {styles.logo} src = {hackmelb_logo}/>
     <div className = {styles.title}>
-      <h2>August 23-25, 2021</h2>
-      <h1>Melbourne Hack</h1>
-      <h3>Melbourne's Premier Hackathon</h3>
+      <FadeIn delay = {290}>
+        <h2>August 23-25, 2021</h2>
+        <h1>Melbourne Hack</h1>
+        <h3>Melbourne's Premier Hackathon</h3>
+      </FadeIn>
     </div>
     <img className = {styles.arrow} src = {arrow_logo}/>
 

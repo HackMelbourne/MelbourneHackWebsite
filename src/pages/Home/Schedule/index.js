c5e72c54cba6c0b8b9369583d17d58a6ab2ea7d3
joann-a <jchin8387@gmail.com>
2021-07-17 15:21:07 +1000

    basic-schedule-page

--- src/pages/Home/Schedule/index.js
+++ src/pages/Home/Schedule/index.js
@@ -4,7 +4,55 @@ import Textbox from "../../../components/Textbox";
 const Schedule = () => {
   return (
     <section id="schedule" className={styles.schedule}>
-      hi
+      <div className={styles.heading}>
+        <h2>Schedule</h2>
+        <div className={styles.underline}></div>
+      </div>
+      <div className={styles.content}>
+        <table className={styles.table}>
+          <tr>
+            <td>Monday</td>
+            <td>8pm</td>
+            <td>Opening Ceremony</td>
+          </tr>
+          <tr className={styles.show_border}>
+            <td></td>
+            <td>10pm</td>
+            <td>Mentor Introduction & First Mentor Check-In</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>10am</td>
+            <td>GitHub Workshop</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>12pm</td>
+            <td>Second Mentor Check-In</td>
+          </tr>
+          <tr className={styles.show_border}>
+            <td></td>
+            <td>12pm</td>
+            <td>Second Mentor Check-In</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>10am</td>
+            <td>HTML/CSS Workshop</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>12pm</td>
+            <td>Third Mentor Check-In</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>8pm</td>
+            <td>Closing Ceremony</td>
+          </tr>
+        </table>
+        <div className={styles.line_feature}></div>
+      </div>
     </section>
   );
 };

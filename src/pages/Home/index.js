537391441f35145c68ea16e013117cbb76121ee6
Ryan <ryan2002.rs@gmail.com>
2021-07-19 20:10:23 +1000

    Add blinking cursor at the end

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,14 +13,14 @@ const Home = () => {
     <div>
       <div className={styles.container}>
         <img className={styles.logo} src={hackmelb_logo}  alt={"Logo"}/>
-        <Row>
+        <Row style={{width:'80vw'}}>
           <Col lg={6}>
             <img className={styles.arrow} src={arrow_logo}  alt={"Arrow Logo"}/>
           </Col>
           <Col lg={6} className={styles.title}>
             <FadeIn delay={290}>
               <h2>August 20-22nd, 2021</h2>
-              <h1>Melbourne Hack</h1>
+              <h1>Melbourne Hack<span className={styles.blinking}>_</span></h1>
               <h3>Melbourne's Premier Hackathon</h3>
             </FadeIn>
           </Col>

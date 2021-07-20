e237c3267b5d56dfe573d5046988f454ecb67418
joann-a <jchin8387@gmail.com>
2021-07-20 13:13:42 +1000

    change stream symbol

--- src/pages/Prizes/index.js
+++ src/pages/Prizes/index.js
@@ -8,31 +8,37 @@ const Prizes = () => {
         <div className={styles.underline}></div>
       </div>
       <div className={styles.content}>
-        <div className={styles.row}>
-          <div className={styles.prize_item}>
-            <h1 className={styles.desktop}>$150</h1>
-            <div className={styles.prize_3_img}></div>
-          </div>
-          <div className={styles.prize_item}>
-            <h1 className={styles.desktop}>$250</h1>
-            <div className={styles.prize_1_img}></div>
-          </div>
-          <div className={styles.prize_item}>
-            <h1 className={styles.desktop}>$200</h1>
-            <div className={styles.prize_2_img}></div>
-          </div>
-          <div className={styles.mobile}>
-            <div>
-              <h3 className={styles.mobile_description}>First place</h3>
-              <h1>$250</h1>
+        <h3>
+          There will be winners for first, second and third place for each of
+          the streams (plus additional prizes too!).{" "}
+        </h3>
+        <div className={styles.podium}>
+          <div className={styles.row}>
+            <div className={styles.prize_item}>
+              <h1 className={styles.desktop}>$200</h1>
+              <div className={styles.prize_2_img}></div>
             </div>
-            <div>
-              <h3 className={styles.mobile_description}>Second place</h3>
-              <h1>$200</h1>
+            <div className={styles.prize_item}>
+              <h1 className={styles.desktop}>$250</h1>
+              <div className={styles.prize_1_img}></div>
             </div>
-            <div>
-              <h3 className={styles.mobile_description}>Third place</h3>
-              <h1>$150</h1>
+            <div className={styles.prize_item}>
+              <h1 className={styles.desktop}>$150</h1>
+              <div className={styles.prize_3_img}></div>
+            </div>
+            <div className={styles.mobile}>
+              <div>
+                <h3 className={styles.mobile_description}>First place</h3>
+                <h1>$250</h1>
+              </div>
+              <div>
+                <h3 className={styles.mobile_description}>Second place</h3>
+                <h1>$200</h1>
+              </div>
+              <div>
+                <h3 className={styles.mobile_description}>Third place</h3>
+                <h1>$150</h1>
+              </div>
             </div>
           </div>
         </div>

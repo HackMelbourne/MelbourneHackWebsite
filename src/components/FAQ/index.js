65d28919c508b9a8ec3e5865e9397820f446db88
ilaylow <chuenleylow@gmail.com>
2021-07-22 00:24:40 +1000

    FAQs done

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -5,10 +5,18 @@ import Question from "./question-ans-component";
 export const FAQ = (props) => {
     return (
         <section className = {styles.faq}>
-            <h2>FAQs</h2>
+            <h2 style = {{fontSize: "250%"}}>FAQs</h2>
             <Question question = "When is the Hackathon happening?" 
             answer = "Yeetus Skeetus"/>
-            <Question question = "When is the Hackathon happening?" 
+            <Question question = "Where is the Hackathon going to be held?" 
+            answer = "Yeetus Skeetus"/>
+            <Question question = "How many people can be in a team?" 
+            answer = "Yeetus Skeetus"/>
+            <Question question = "I don't have a team, can I sign up as an individual?" 
+            answer = "Yeetus Skeetus"/>
+            <Question question = "How many people can be in a team?" 
+            answer = "Yeetus Skeetus"/>
+            <Question question = "When is the deadline for applications?" 
             answer = "Yeetus Skeetus"/>
         </section>
     )
--- src/components/FAQ/question-ans-component.js
+++ src/components/FAQ/question-ans-component.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./questions.css";
-import CSSTransitionGroup from 'react-addons-css-transition-group';
+import styles from "./questions.module.scss";
+import dropdown_arrow from "../assets/down-arrow.png"
 import SlideToggle from "react-slide-toggle";
 
 export default class Question extends React.Component{
@@ -22,11 +22,12 @@ export default class Question extends React.Component{
                 <SlideToggle duration = {500} collapsed
                 render = {({toggle, setCollapsibleElement, progress}) => (
                     <div>
-                        <div onClick = {toggle} className = "question">
-                            <h3>{this.props.question}</h3>
+                        <div onClick = {toggle} className = {styles.question}>
+                            <span style = {{fontSize: "150%"}}>{this.props.question}</span>
+                            <img src = {dropdown_arrow} alt = "Dropdown arrow"/>
                         </div>
                         <div ref={setCollapsibleElement}>
-                            <div className = "answer">
+                            <div className = {styles.answer}>
                                 <h3>{this.props.answer}</h3>
                             </div>
                         </div>

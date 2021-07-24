861869096ae79e3c59db8d690910f4ebebc0df7e
jonjau <jonjau3q@gmail.com>
2021-07-24 17:08:32 +1000

    changed mobile menu styles

--- src/components/Navbar/Menu/index.js
+++ src/components/Navbar/Menu/index.js
@@ -1,19 +1,22 @@
 import styles from "./Menu.module.scss";
+import { Link } from "react-router-dom";
 
 // Menu has no dropdown functionality - can't hover on mobile
 const Menu = ({ open }) => {
   return (
     <nav className={open ? `${styles.menu} ${styles.open}` : styles.menu}>
       <a
-        href="https://www.facebook.com/events/426944495051069/?active_tab=discussion"
+        href="https://www.facebook.com/"
         target="_blank"
         rel="noopener noreferrer"
         className={styles.link}
       >
         UPDATES
       </a>
+      <Link to="/Prizes">PRIZES</Link>
+      <Link to="/Sponsors">SPONSORS</Link>
       <a
-        href="https://forms.gle/JuhBU1fiFLwe8HzH8"
+        href="https://forms.gle/"
         target="_blank"
         rel="noopener noreferrer"
       >

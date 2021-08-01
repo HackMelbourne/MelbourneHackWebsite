638eafa52f8bc9ea75ba2d3f4cbce4dba78ed3a0
Serena Wang <serenaw@student.unimelb.edu.au>
2021-08-02 03:00:45 +1000

    add titles to sterams

--- src/components/Streams/streams.js
+++ src/components/Streams/streams.js
@@ -23,23 +23,23 @@ export const Streams = () => {
         let description;
         switch(stream) {
             case 'finance':
-                description = <p>In a world that revolves around money, finance plays an integral part in an individual's daily life. The finance track is aimed at participants who would like to develop a final project that helps individuals ranging from those who are a part of the finance community or perhaps those trying to understand the world of finance.  What can you contribute to the world of finance?</p>
+                description = <p><h1>Finance</h1><br/>In a world that revolves around money, finance plays an integral part in an individual's daily life. The finance track is aimed at participants who would like to develop a final project that helps individuals ranging from those who are a part of the finance community or perhaps those trying to understand the world of finance.  What can you contribute to the world of finance?</p>
                 setDescription(description);
                 break;
             case 'medTech':
-                description = <p>Rapidly evolving technology has made many of today's advancements possible, especially in the field of medical technology. The Medtech field is targeted towards participants who wish to create a project to contribute to this fast-growing medical technology field. The final project can be aimed to help those who actively work in the medical field, or perhaps those who require medical assistance. How do you wish to be a part of the medical technology evolution?</p>
+                description = <p><h1>MedTech</h1><br/>Rapidly evolving technology has made many of today's advancements possible, especially in the field of medical technology. The Medtech field is targeted towards participants who wish to create a project to contribute to this fast-growing medical technology field. The final project can be aimed to help those who actively work in the medical field, or perhaps those who require medical assistance. How do you wish to be a part of the medical technology evolution?</p>
                 setDescription(description);
                 break;
             case 'education':
-                description = <p>Education plays a crucial role in enriching our lives, allowing us to gain knowledge or learn new skills. This track is geared towards participants who wish to improve the lives of others through education, with their final project being targeted towards helping educators or assisting those who wish to educate themselves. How do you want to shape the future of education?</p>
+                description = <p><h1>Education</h1><br/>Education plays a crucial role in enriching our lives, allowing us to gain knowledge or learn new skills. This track is geared towards participants who wish to improve the lives of others through education, with their final project being targeted towards helping educators or assisting those who wish to educate themselves. How do you want to shape the future of education?</p>
                 setDescription(description);
                 break;
             case 'sustainability':
-                description = <p>Due to the rapid escalation of global warming, sustainability has become much more relevant today than ever before. The sustainability track is for those who wish to create a project to help fight against environmental deterioration and to combat climate change with the help of technology. How would you like to respond to the ongoing environmental crisis?</p>
+                description = <p><h1>Sustainability</h1><br/>Due to the rapid escalation of global warming, sustainability has become much more relevant today than ever before. The sustainability track is for those who wish to create a project to help fight against environmental deterioration and to combat climate change with the help of technology. How would you like to respond to the ongoing environmental crisis?</p>
                 setDescription(description);
                 break;
             case 'meme':
-                description = <p>Not every project needs to save the world... sometimes, we just do projects for fun🙈
+                description = <p><h1>Meme</h1><br/>Not every project needs to save the world... sometimes, we just do projects for fun🙈
 
                     <br/>We're excited to see your creativity put to the test by developing the most meme-iest, outlandish product yet. If it makes the judges giggle, then it's a job well done.
 

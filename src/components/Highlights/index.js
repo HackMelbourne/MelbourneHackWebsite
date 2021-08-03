b4d4fb749e8514cb0dab40d24aaafa39fad32871
jonjau <jonjau3q@gmail.com>
2021-08-03 12:13:37 +1000

    change highlights text, centering, and add bottom margin

--- src/components/Highlights/index.js
+++ src/components/Highlights/index.js
@@ -1,5 +1,4 @@
 import styles from "./Highlights.module.scss";
-import Textbox from "../Textbox";
 import image1 from "../assets/Highlight icons/19.svg";
 import image2 from "../assets/Highlight icons/20.svg";
 import image3 from "../assets/Highlight icons/21.svg";
@@ -18,37 +17,34 @@ const Highlights = () => {
         <Grid className = {styles.grid} container item xs = {11} spacing = {2}>
           <div className = {styles.content}>
             <img src = {image1} alt = "" style = {{backgroundColor: "rgba(255, 255, 10, 0.15)"}}/>
-            <h3>Some Text Here...</h3>
+            <h3>workshops</h3>
           </div>
           <div className = {styles.content2}>
             <img src = {image2} alt = "" style = {{backgroundColor: "rgba(0, 221, 255, 0.15)"}}/>
-            <h3>Some Text Here...</h3>
+            <h3>panel events</h3>
           </div>
         </Grid>
         <Grid className = {styles.grid} container item xs = {11} spacing = {2}>
           <div className = {styles.content}>
             <img src = {image3} alt = "" style = {{backgroundColor: "rgba(255, 51, 51, 0.15)"}}/>
-            <h3>Some Text Here...</h3>
+            <h3>networking night</h3>
           </div>
           <div className = {styles.content2}>
             <img src = {image4} alt = "" style = {{backgroundColor: "rgba(160, 51, 51, 0.15)"}}/>
-            <h3>Some Text Here...</h3>
+            <h3>keynote speakers</h3>
           </div>
         </Grid>
         <Grid className = {styles.grid} container item xs = {11} spacing = {2}>
           <div className = {styles.content}>
             <img src = {image5} alt = "" style = {{backgroundColor: "rgba(0, 128, 255, 0.15)"}}/>
-            <h3>Some Text Here...</h3>
+            <h3>mentors</h3>
           </div>
           <div className = {styles.content2}>
             <img src = {image6} alt = "" style = {{backgroundColor: "rgba(51, 255, 153, 0.15)"}}/>
-            <h3>Some Text Here...</h3>
+            <h3>social night</h3>
           </div>
         </Grid>
-        
-
       </Grid>
-
     </section>
   );
 };

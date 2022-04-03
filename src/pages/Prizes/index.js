0ace6f0e3fc1ffd567dbb92cd38a8bf073490446
Florence Tang <fytang@student.unimelb.edu.au>
2022-04-03 21:26:40 +1000

    Update Prize desc, icons and Theme desc

--- src/pages/Prizes/index.js
+++ src/pages/Prizes/index.js
@@ -10,28 +10,32 @@ const Prizes = () => {
             <h2>
               Prizes<span className={styles.blinking}>_</span>
             </h2>
+            <h3>Have you got a competitive streak? Check out our awesome prizes below!</h3>
           </div>
           <Grid container>
             <Grid xs={12} md={4} className={styles.prize_circle}>
-              <div className={styles.circle}></div>
-              <div className={styles.circle_text}>
+              
+              
+                <img alt='1st' className = {styles.PrizeMedal} src = "1st.svg"/>
                 <h4>1st place</h4>
                 <h5>$1500</h5>
-              </div>
+              
             </Grid>
             <Grid xs={12} md={4} className={styles.prize_circle}>
-              <div className={styles.circle}></div>
-              <div className={styles.circle_text}>
+              
+              
+                <img alt='2nd' className = {styles.PrizeMedal} src = "2nd.svg"/>
                 <h4>2nd place</h4>
                 <h5>$1000</h5>
-              </div>
+             
             </Grid>
             <Grid xs={12} md={4} className={styles.prize_circle}>
-              <div className={styles.circle}></div>
-              <div className={styles.circle_text}>
+              
+             
+                <img alt='3rd' className = {styles.PrizeMedal} src = "3rd.svg"/>
                 <h4>3rd place</h4>
                 <h5>$1500</h5>
-              </div>
+            
             </Grid>
           
           </Grid>
@@ -41,8 +45,7 @@ const Prizes = () => {
             <div className={styles.prize_circle}>
               <div className={styles.circle}></div>
               <div className={styles.circle_text}>
-               <h4>Other Swag</h4>
-               <h4>and Goodies</h4>
+               <h4>Other Swag and Goodies</h4>
               </div>
             </div>
       

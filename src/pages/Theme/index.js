36ac7999d14e97d54b84fde813e44b9a1ea66d5f
Ryan <ryan2002.rs@gmail.com>
2022-04-03 17:14:48 +1000

    QUickly update theme

--- src/pages/Theme/index.js
+++ src/pages/Theme/index.js
@@ -4,12 +4,7 @@ const Theme = () => {
     <div className={styles.Theme}>
 
         <h2>The Theme<span className={styles.blinking}>_</span></h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
+        <p>The theme for this will be <b>EduTech</b></p>
     </div>
   );
 };

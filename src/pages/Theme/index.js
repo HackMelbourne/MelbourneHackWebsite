fc1bd9bc6c9ddc25ed71fd8b7e4a6eb253baaf10
Ryan Samarakoon <ryan2002.rs@gmail.com>
2022-04-12 17:09:38 +1000

    Fix edutech not centered

--- src/pages/Theme/index.js
+++ src/pages/Theme/index.js
@@ -1,8 +1,7 @@
 import styles from "./Theme.module.scss";
 const Theme = () => {
   return (
-    <div className={styles.Theme}>
-      <section id="theme">
+    <div className={styles.Theme} id='theme'>
         <h2>The Theme<span className={styles.blinking}>_</span></h2>
         <img alt='educationtech' src = "edutech.png" height="300"/>
         <h3>EduTech</h3>
@@ -13,7 +12,6 @@ const Theme = () => {
           to formulate innovative, forward-thinking solutions that can make a positive impact 
           on the lives of students, teachers and other stakeholders!
         </p>
-      </section>
     </div>
   );
 };

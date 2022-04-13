5a09f776852aba8dba94d3556b96402dd65c9719
chuahx <xinyu.chuah@student.unimelb.edu.au>
2022-04-13 21:01:47 +1000

    Added IntegraDev Sponsor

--- src/pages/Sponsors/index.js
+++ src/pages/Sponsors/index.js
@@ -1,6 +1,7 @@
 import styles from "./Sponsors.module.scss";
 import {Grid} from "@material-ui/core";
 import redMarble from '../../components/assets/sponsors/red_marble.png';
+import integraDev from '../../components/assets/sponsors/integradev/integradev.png';
 
 const Sponsors = () => {
     return(<div className={styles.sponsors}>
@@ -15,6 +16,11 @@ const Sponsors = () => {
                 <h3>Platinum</h3>
                 <img alt='star' src = "Star 3.png"/>
                 <img alt='logo' className = {styles.Optiver} src = "Optiver.png"/>
+            </div></Grid>
+            <Grid item xs={12} md={4}><div>
+                <h3>Gold</h3>
+                <img alt='star' src = "Star 2.png"/>
+                <img alt='logo' className = {styles.Optiver} src = "Bain And Company.png"/>
             </div></Grid>
             <Grid item xs={12} md={4}><div>
                 <h3>Gold</h3>
